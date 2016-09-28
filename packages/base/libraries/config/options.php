1dbdec0fdc931aa4d7521905945a9b457f8601b2
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2016-09-28 09:48:57 +0330

    fix options and use json instance of serialize

--- packages/base/libraries/config/options.php
+++ packages/base/libraries/config/options.php
@@ -1,15 +1,20 @@
 <?php
 namespace packages\base;
 use \packages\base\db;
+use \packages\base\json;
 class options{
 	private static $options = array();
 	static function load($option, $reload = false){
 		if($reload or !isset(self::$options[$option])){
 			loader::requiredb();
 			db::where("name", $option);
-			if($value = db::get("options", "value")){
-				$value = serialize($value);
+			if($value = db::getValue("options", "value")){
+				$fchar = substr($value, 0,1);
+				if($fchar == '{' or $fchar == '['){
+					$value = json\decode($value);
+				}
 				self::$options[$option] = $value;
+				return $value;
 			}
 		}else{
 			return self::$options[$option];
@@ -21,7 +26,6 @@ class options{
 		return true;
 	}
 	static function get($option){
-		return isset(self::$options[$option]) ? self::$options[$option] : false;
+		return isset(self::$options[$option]) ? self::$options[$option] : self::load($option);
 	}
 }
-?>
