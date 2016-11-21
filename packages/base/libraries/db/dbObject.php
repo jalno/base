484ae17c6b905b3af427170f1bba25db0ac4dcd0
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2016-11-21 12:54:13 +0330

    Fix #25

--- packages/base/libraries/db/dbObject.php
+++ packages/base/libraries/db/dbObject.php
@@ -159,21 +159,27 @@ class dbObject {
 			$modelName = $this->relations[$name][1];
 			switch ($relationType) {
 				case 'hasone':
-					$key = isset ($this->relations[$name][2]) ? $this->relations[$name][2] : $name;
-					$obj = new $modelName;
-					$obj->returnType = $this->returnType;
-					return $this->data[$name] = $obj->byId($this->data[$key]);
+					if(isset($this->data[$key])){
+						$key = isset ($this->relations[$name][2]) ? $this->relations[$name][2] : $name;
+						$obj = new $modelName;
+						$obj->returnType = $this->returnType;
+						return $this->data[$name] = $obj->byId($this->data[$key]);
+					}
+					return null;
 					break;
 				case 'hasmany':
-					$key = $this->relations[$name][2];
-					$obj = new $modelName;
-					$obj->returnType = $this->returnType;
-					$obj->where($key, $this->data[$this->primaryKey]);
-					$this->data[$name] = $obj->get();
-					if(!$this->data[$name]){
-						$this->data[$name] = array();
+					if(isset($this->data[$this->primaryKey])){
+						$key = $this->relations[$name][2];
+						$obj = new $modelName;
+						$obj->returnType = $this->returnType;
+						$obj->where($key, $this->data[$this->primaryKey]);
+						$this->data[$name] = $obj->get();
+						if(!$this->data[$name]){
+							$this->data[$name] = array();
+						}
+						return $this->data[$name];
 					}
-					return $this->data[$name];
+					return array();
 					break;
 				default:
 					break;
