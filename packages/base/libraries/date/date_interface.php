660e00f4c9f3b746765ddd1fbe596b44a36dca5c
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2017-03-08 23:49:19 +0330

    remove is_dst as php 7

--- packages/base/libraries/date/date_interface.php
+++ packages/base/libraries/date/date_interface.php
@@ -3,5 +3,5 @@ namespace packages\base\date;
 interface date_interface{
 	public static function format($format ,$timestamp = null);
 	public static function strtotime($time,$now = null);
-	public static function mktime($hour = null, $minute = null, $second = null , $month = null, $day = null, $year = null, $is_dst = -1);
+	public static function mktime($hour = null, $minute = null, $second = null , $month = null, $day = null, $year = null);
 }
