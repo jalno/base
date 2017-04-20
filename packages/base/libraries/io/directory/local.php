aaaa64a490af0789b69f80001bcaf271b6ccca39
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2017-04-20 16:48:35 +0430

    define copyForm for local directory

--- packages/base/libraries/io/directory/local.php
+++ packages/base/libraries/io/directory/local.php
@@ -59,7 +59,7 @@ class local extends directory{
 		return true;
     }
     public function copyFrom(directory $source): bool{
-        
+        return $source->copyTo($this);
     }
     public function files(bool $recursively = true):array{
         $scanner = function($dir) use($recursively, &$scanner){
