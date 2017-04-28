7999b5b19b48f26fb48f74912f845a4c649dddb9
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2017-04-28 14:28:24 +0430

    add sftp file and directory

---
+++ packages/base/libraries/io/directory/sftp.php
@@ -0,0 +1,221 @@
+<?php
+namespace packages\base\IO\directory;
+use \packages\base\ssh;
+use \packages\base\IO\file;
+use \packages\base\IO\directory;
+use \packages\base\IO\drivers\sftp as driver;
+use \packages\base\IO\NotFoundException;
+class sftp extends directory{
+    public $hostname;
+	public $port;
+	public $username;
+	public $password;
+	private $driver;
+	public function setDriver(driver $driver){
+		$this->driver = $driver;
+	}
+	public function getDriver():driver{
+		if($this->driver){
+			return $this->driver;
+		}
+		$ssh = new ssh($this->hostname, $this->port);
+		if(!$ssh->AuthByPassword($this->username, $this->password)){
+			throw new Exeption();
+		}
+		$this->driver = new driver($ssh);
+		return $this->driver;
+	}
+    public function size(): int{
+        $size = 0;
+        foreach($this->files(true) as $file){
+            $size += $file->size();
+        }
+        return $size;
+    }
+    public function move(directory $dest): bool{
+        if(!$dest->exists()){
+            $dest->make(true);
+        }
+        if($this->getDriver()->rename($this->getPath(), $dest->getPath().'/'.$this->basename)){
+            $this->directory = $dest->getPath();
+            return true;
+        }
+        return false;
+    }
+    public function rename(string $newName): bool{
+        if($this->getDriver()->rename($this->getPath(), $this->directory.'/'.$newName)){
+            $this->basename = $newName;
+            return true;
+        }
+        return false;
+    }
+    public function delete(){
+        foreach($this->items(false) as $item){
+			$item->delete();
+		}
+		$this->getDriver()->rmdir($this->getPath());
+    }
+    public function make(bool $recursive = false):bool{
+        $driver = $this->getDriver();
+        if($recursive){
+            $dirs = explode('/', $this->getPath());
+            $dir='';
+            foreach ($dirs as $part) {
+               	$dir .= $part.'/';
+                if ($part){
+					if(!$driver->is_dir($dir)){
+						if(!$driver->mkdir($dir)){
+							return false;
+						}
+					}
+				}
+            }
+            return true;
+        }else{
+            return $driver->mkdir($this->getPath());
+        }
+    }
+    public function copyTo(directory $dest): bool{
+		$sourcePath = $this->getPath();
+		if(!$dest->exists()){
+			$dest->make(true);
+		}
+		$items = $this->items(true);
+		foreach($items as $item){
+			$relativePath = substr($item->getPath(), strlen($sourcePath)+1);
+			if($item instanceof file){
+				if(!$item->copyTo($dest->file($relativePath))){
+					return false;	
+				}
+			}else{
+				$destDir = $dest->directory($relativePath);
+				if(!$destDir->exists()){
+					if(!$destDir->make(true)){
+						return false;	
+					}
+				}
+			}
+		}
+		return true;
+	}
+	public function copyFrom(directory $source): bool{
+		return $source->copyTo($this);
+	}
+
+    
+	public function files(bool $recursively = false):array{
+		$driver = $this->getDriver();
+		$scanner = function($dir) use($recursively, $driver, &$scanner){
+			$items = [];
+            $handle = $driver->opendir($dir);
+            while (($basename = readdir($handle)) !== false) {
+                if($basename != '.' and $basename != '..'){
+                    $item = $dir.'/'.$basename;
+                    if($driver->is_file($item)){
+                        $file = new file\sftp($item);
+						$file->setDriver($driver);
+						$items[] = $file;
+                    }elseif($recursively and $driver->is_dir($item)){
+						$items = array_merge($items, $scanner($item));
+					}
+                }
+            }
+            return $items;
+		};
+		return $scanner($this->getPath());
+	}
+	public function directories(bool $recursively = true):array{
+		$driver = $this->getDriver();
+        $scanner = function($dir) use($recursively, $driver, &$scanner){
+			$items = [];
+            $handle = $driver->opendir($dir);
+            while (($basename = readdir($handle)) !== false) {
+                if($basename != '.' and $basename != '..'){
+                    $item = $dir.'/'.$basename;
+                    if($driver->is_dir($item)){
+                        $directory = new directory\sftp($item);
+						$directory->setDriver($driver);
+						$items[] = $directory;
+                        if($recursively){
+                            $items = array_merge($items, $scanner($item));
+                        }
+                    }
+                }
+            }
+            return $items;
+		};
+		return $scanner($this->getPath());
+	}
+	public function items(bool $recursively = true):array{
+        $driver = $this->getDriver();
+        $scanner = function($dir) use($recursively, $driver, &$scanner){
+			$items = [];
+            $handle = $driver->opendir($dir);
+            while (($basename = readdir($handle)) !== false) {
+                if($basename != '.' and $basename != '..'){
+                    $item = $dir.'/'.$basename;
+                    if($driver->is_file($item)){
+                        $file = new file\sftp($item);
+						$file->setDriver($driver);
+						$items[] = $file;
+                    }elseif($driver->is_dir($item)){
+                        $directory = new directory\sftp($item);
+						$directory->setDriver($driver);
+						$items[] = $directory;
+                        if($recursively){
+                            $items = array_merge($items, $scanner($item));
+                        }
+                    }
+                }
+            }
+            return $items;
+		};
+		return $scanner($this->getPath());
+	}
+
+	public function exists():bool{
+		return $this->getDriver()->is_dir($this->getPath());
+	}
+	public function file(string $name):file\sftp{
+		$file = new file\sftp($this->getPath().'/'.$name);
+		$file->setDriver($this->getDriver());
+		return $file;
+	}
+	public function directory(string $name):directory\sftp{
+		$directory = new directory\sftp($this->getPath().'/'.$name);
+		$directory->setDriver($this->getDriver());
+		return $directory;
+	}
+	public function serialize(){
+		if(!$this->hostname){
+			$this->hostname = $this->getDriver()->getSSH()->getHost();
+		}
+		if(!$this->port){
+			$this->port = $this->getDriver()->getSSH()->getPort();
+		}
+		if(!$this->username){
+			$this->username = $this->getDriver()->getSSH()->getUsername();
+		}
+		if(!$this->password){
+			$this->password = $this->getDriver()->getSSH()->getPassword();
+		}
+		$data = array(
+			'directory' => $this->directory,
+			'basename' => $this->basename,
+			'hostname' => $this->hostname,
+			'port' => $this->port,
+			'username' => $this->username,
+			'password' => $this->password
+		);
+        return serialize($data);
+    }
+	public function unserialize($data){
+		$data = unserialize($data);
+		$this->directory = isset($data['directory']) ? $data['directory'] : null;
+		$this->basename = isset($data['basename']) ? $data['basename'] : null;
+		$this->hostname = isset($data['hostname']) ? $data['hostname'] : null;
+		$this->port = isset($data['port']) ? $data['port'] : 21;
+		$this->username = isset($data['username']) ? $data['username'] : null;
+		$this->password = isset($data['password']) ? $data['password'] : null;
+	}
+}
---
+++ packages/base/libraries/io/file/sftp.php
@@ -0,0 +1,122 @@
+<?php
+namespace packages\base\IO\file;
+use \packages\base\ssh;
+use \packages\base\IO\file;
+use \packages\base\IO\directory;
+use \packages\base\IO\file\local;
+use \packages\base\IO\drivers\sftp as driver;
+use \packages\base\IO\ReadException;
+class sftp extends file{
+	public $hostname;
+	public $port;
+	public $username;
+	public $password;
+	private $driver;
+	public function setDriver(driver $driver){
+		$this->driver = $driver;
+	}
+	public function getDriver():driver{
+		if($this->driver){
+			return $this->driver;
+		}
+		$ssh = new ssh($this->hostname, $this->port);
+		if(!$ssh->AuthByPassword($this->username, $this->password)){
+			throw new Exeption();
+		}
+		$this->driver = new driver($ssh);
+		return $this->driver;
+	}
+	public function open(string $mode):buffer {
+        return $this->getDriver()->open($this->getPath(), $mode);
+    }
+	public function write(string $data):bool{
+		return $this->getDriver()->put_contents($this->getPath(), $data);
+	}
+	public function read(int $length = 0):string {
+		if($length == 0){
+			return $this->getDriver()->get_contents($this->getPath());
+		}
+		$buffer = $this->open('r');
+		return $buffer->read($length);
+	}
+	public function size(): int{
+		return $this->getDriver()->size($this->getPath());
+	}
+	public function move(file $dest): bool{
+		if($dest instanceof self){
+			return $this->getDriver()->rename($this->getPath(), $dest->getPath());
+		}
+	}
+	public function rename(string $newName): bool{
+		return $this->getDriver()->rename($this->getPath(), $this->directory.'/'.$newName);
+	}
+	public function delete(){
+		$this->getDriver()->unlink($this->getPath());
+	}
+	public function copyTo(file $dest): bool{
+		$driver = $this->getDriver();
+		if($dest instanceof self){
+			$tmp = new tmp();
+			if($this->copyTo($tmp)){
+				return $dest->copyFrom($tmp);
+			}
+		}elseif($dest instanceof local){
+			return $driver->download($this->getPath(), $dest->getPath());
+		}
+	}
+	public function copyFrom(file $source): bool{
+		if($source instanceof self){
+			$tmp = new tmp();
+			if($source->copyTo($tmp)){
+				return $this->copyFrom($tmp);
+			}
+		}elseif($source instanceof local){
+			return $this->getDriver()->upload($source->getPath(), $this->getPath());
+		}
+	}
+	public function getDirectory():directory\sftp{
+		$directory = new directory\sftp($this->directory);
+		$directory->setDriver($this->getDriver());
+		return $directory;
+	}
+    public function serialize(){
+		$driver = $this->getDriver();
+		$data = array(
+			'directory' => $this->directory,
+			'basename' => $this->basename
+		);
+		if($this->hostname){
+			$data['hostname'] = $this->hostname;
+		}elseif($driver){
+			$data['hostname'] = $driver->getSSH()->getHost();
+		}
+
+		if($this->port){
+			$data['port'] = $this->port;
+		}elseif($driver){
+			$data['port'] = $driver->getSSH()->getPort();
+		}
+
+		if($this->username){
+			$data['username'] = $this->username;
+		}elseif($driver){
+			$data['username'] = $driver->getSSH()->getUsername();
+		}
+
+		if($this->password){
+			$data['password'] = $this->password;
+		}elseif($driver){
+			$data['password'] = $driver->getSSH()->getPassword();
+		}
+        return serialize($data);
+    }
+    public function unserialize($data){
+		$data = unserialize($data);
+		$this->username = isset($data['username']) ? $data['username'] : null;
+		$this->password = isset($data['password']) ? $data['password'] : null;
+		$this->port = isset($data['port']) ? $data['port'] : null;
+		$this->hostname = isset($data['hostname']) ? $data['hostname'] : null;
+		$this->directory = isset($data['directory']) ? $data['directory'] : null;
+		$this->basename = isset($data['basename']) ? $data['basename'] : null;
+    }
+}
