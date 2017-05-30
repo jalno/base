7055cd193461165814bf1ef5b44e33c991e444d5
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2017-05-30 16:37:29 +0430

    logging for events trigger

--- packages/base/libraries/events/events.php
+++ packages/base/libraries/events/events.php
@@ -3,6 +3,8 @@ namespace packages\base;
 use \packages\base\frontend\theme;
 class events{
 	static public function trigger(event $event){
+		$log = log::getInstance();
+		$log->debug("trigger", get_class($event));
 		foreach(packages::get() as $package){
 			$package->trigger($event);
 		}
