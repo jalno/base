de1e1a8ba714a6171863ec8c20ecddf4adc0be64
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2017-06-20 11:41:38 +0430

    Fix #30

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 error_reporting(E_ALL);
 ini_set( 'display_startup_errors', 1 );
 ini_set('display_errors', 1);
+chdir(__DIR__);
 use \packages\base\loader;
 use \packages\base\router;
 use \packages\base\http;
