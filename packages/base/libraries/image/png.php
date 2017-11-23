c74ab8cb130043aebcf49b1e426981daa697261a
Mahdi Abedi <abedi@jeyserver.com>
2017-11-23 11:59:59 +0330

    fix png transparent background

--- packages/base/libraries/image/png.php
+++ packages/base/libraries/image/png.php
@@ -8,5 +8,17 @@ class png extends gd{
 	}
 	public function saveToFile(file $file, int $quality = 75){
 		imagepng($this->image, $file->getPath(), round((100 - $quality) / 10));
-	} 
+	}
+
+	protected function createBlank(int $width, int $height, color $bg){
+		$this->image = imagecreatetruecolor($width, $height);
+		$colors = $bg->toRGBA();
+		$colors[3] = round(127 - ($colors[3] * 127));
+		$rgba = imagecolorallocatealpha($this->image, $colors[0], $colors[1], $colors[2], $colors[3]);
+		imagecolortransparent($this->image, $rgba);
+		imageAlphaBlending($this->image, false);
+		imageSaveAlpha($this->image, true);
+
+		imagefilledrectangle($this->image, 0, 0, $width, $height, $rgba);
+	}
 }
