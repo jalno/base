a5d5ecf993dd19093e3621cb9eb439fb71daa425
Amir.h Yeganemehr <yeganemehr@yahoo.com>
2019-02-14 20:22:42 +0330

    Rewrite autoloader

---
+++ packages/base/libraries/autoloader/Autoloader.php
@@ -0,0 +1,171 @@
+<?php
+namespace packages\base;
+
+use packages\PhpParser\{ParserFactory, NodeTraverser, Node, NodeVisitorAbstract};
+
+class Autoloader {
+	private static $classes = array();
+
+	/**
+	 * Register autoloader as __autoload() implementation.
+	 * 
+	 * @return void
+	 */
+	public static function register() {
+		spl_autoload_register(Autoloader::class . "::handler");
+	}
+	/**
+	 * Parse a php file and return list of autoloader items.
+	 * 
+	 * @param packages\base\IO\file $file must be exist.
+	 * @return string[] 
+	 */
+	public static function getAutoloaderItemsFromFile(IO\file $file): array {
+		$visitor = new class extends NodeVisitorAbstract {
+			/**
+			 * @var array items which found by enterNode() method.
+			 */
+			protected $items = [];
+		
+			/**
+			 * @var string current namespace in file.
+			 */
+			protected $namespace;
+		
+			/**
+			 * Called after enter into every node and if it was a class or trait or interface we member it so later we able to generate a autoloader record.
+			 * 
+			 * @param packages\PhpParser\Node $node
+			 * @return void
+			 */
+			public function enterNode(Node $node) {
+				if(
+					$node instanceof Node\Stmt\Class_ or
+					$node instanceof Node\Stmt\Trait_ or
+					$node instanceof Node\Stmt\Interface_
+				) {
+					$this->items[] = ($this->namespace ? $this->namespace."\\" : "").$node->name;
+				} elseif ($node instanceof Node\Stmt\Namespace_) {
+					$this->namespace = implode("\\", $node->name->parts);
+				}
+			}
+			
+			/**
+			 * Get items which found by enterNode() method.
+			 *
+			 * @return array
+			 */ 
+			public function getItems(): array {
+				return $this->items;
+			}
+		};
+		$traverser = new NodeTraverser;
+		$traverser->addVisitor($visitor);
+		$parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+		try {
+			$stmts = $parser->parse($file->read());
+			$traverser->traverse($stmts);
+		} catch(\Expcetion $e) {}
+		return $visitor->getItems();
+	}
+
+	/**
+	 * Register a peer class-file
+	 * 
+	 * @param string $class Fully-qualified class name
+	 * @param string $file file path. must be exist.
+	 * @return void
+	 */
+	public static function addClass(string $class, string $file): void {
+		$class = ltrim($class, "\\");
+		if (isset(self::$classes[$class])) {
+			return;
+		}
+		self::$classes[$class] = $file;
+	}
+
+	/**
+	 * Remove a class from registery.
+	 * 
+	 * @param string $class Fully-qualified class name
+	 * @return void
+	 */
+	public static function removeClass(string $class){
+		$class = ltrim($class, "\\");
+		unset(self::$classes[$class]);
+	}
+
+	/**
+	 * Handle requests from php engine and recall the file of requested file.
+	 * 
+	 * @param string $class Fully-qualified class name
+	 * @return void
+	 */
+	public static function handler(string $class): void {
+		if (isset(self::$classes[$class])) {
+			require_once(self::$classes[$class]);
+		}
+	}
+	
+	public static function getParentList(bool $cache): array {
+		static $items;
+		if (isset($items) and $items) {
+			return $items;
+		}
+		if ($cache) {
+			$items = cache::get("packages.base.autoloader.parentList-" . implode("", array_keys(self::$classes)));
+			if ($items) {
+				return $items;
+			}
+		}
+		foreach (self::$classes as $file) {
+			require_once($file);
+		}
+		$isJalno = function(string $class): bool {
+			return (substr($class, 0, 9) == "packages\\" or substr($class, 0, 7) == "themes\\");
+		};
+		$classes = get_declared_classes();
+		$items = [];
+		foreach ($classes as $class) {
+			if (!$isJalno($class) or !isset(self::$classes[$class])) {
+				continue;
+			}
+			if (!isset($items[$class])) {
+				$items[$class] = array(
+					'file' => self::$classes[$class],
+				);
+			}
+
+			$parents = array_merge(class_implements($class, false) ?? [], [get_parent_class($class)]);
+			foreach ($parents as $parent) {
+				if (!$parent or !$isJalno($parent)) {
+					continue;
+				}
+				if (isset($items[$parent])) {
+					if (!isset($items[$parent]['children'])) {
+						$items[$parent]['children'] = [];
+					}
+					$items[$parent]['children'][] = $class;
+				} elseif (isset(self::$classes[$parent])) {
+					$items[$parent] = array(
+						'file' => self::$classes[$parent],
+						'children' => array($class)
+					);
+				}
+			}
+		}
+		if ($items and $cache) {
+			cache::set("packages.base.autoloader.parentList-" . implode("", array_keys(self::$classes)), $items, 0);
+		}
+		return $items;
+	}
+
+	/**
+	 * Return existance status of php parser.
+	 * 
+	 * @return bool
+	 */
+	public static function canParsePHP(): bool {
+		return class_exists("packages\\PhpParser\\ParserFactory");
+	}
+}
