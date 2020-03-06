f3c8a2a4c90eb7682013df98751ab89d787c6502
Hossein Hosni <hosni.hossein@gmail.com>
2020-03-07 00:46:07 +0330

    Fix #45
    Add Log coloring

--- packages/base/libraries/logging/log.php
+++ packages/base/libraries/logging/log.php
@@ -1,7 +1,9 @@
 <?php
 namespace packages\base;
-use \packages\base\log\instance;
-class log{
+
+use packages\base\log\Instance;
+
+class Log {
 	const debug = 1;
 	const info = 2;
 	const warn = 3;
@@ -12,77 +14,101 @@ class log{
 	static protected $file;
 	static private $generation = 0;
 	static private $indentation = "\t";
-	public static function newChild(){
+	public static function newChild() {
 		self::$generation++;
 	}
-	public static function dieChild(){
+	public static function dieChild() {
 		self::$generation--;
 	}
-	public static function getParent(){
-		if(!self::$parent){
+	public static function getParent() {
+		if (!self::$parent) {
 			self::$parent = self::getInstance();
 		}
 		return self::$parent;
 	}
-	public static function getInstance(){
+	public static function getInstance() {
 		$level = self::off;
-		if(self::$parent){
+		if (self::$parent) {
 			$level = self::$parent->getLevel();
 		}
 		return new instance($level);
 	}
-	public static function setFile($file){
+	public static function setFile($file) {
 		self::$file = $file;
 	}
-	public static function setLevel($level){
-		switch(strtolower($level)){
-			case('debug'):$level = self::debug;break;
-			case('info'):$level = self::info;break;
-			case('warn'):$level = self::warn;break;
-			case('error'):$level = self::error;break;
-			case('fatal'):$level = self::fatal;break;
-			case('off'):$level = self::off;break;
+	public static function setLevel($level) {
+		switch(strtolower($level)) {
+			case('debug'):$level = self::debug; break;
+			case('info'):$level = self::info; break;
+			case('warn'):$level = self::warn; break;
+			case('error'):$level = self::error; break;
+			case('fatal'):$level = self::fatal; break;
+			case('off'):$level = self::off; break;
 		}
 		self::getParent()->setLevel($level);
 	}
-	public static function debug(){
+	public static function debug() {
 		return call_user_func_array(array(self::getParent(),'debug'), func_get_args());
 	}
-	public static function info(){
+	public static function info() {
 		return call_user_func_array(array(self::getParent(),'info'), func_get_args());
 	}
-	public static function warn(){
+	public static function warn() {
 		return call_user_func_array(array(self::getParent(),'warn'), func_get_args());
 	}
-	public static function error(){
+	public static function error() {
 		return call_user_func_array(array(self::getParent(),'error'), func_get_args());
 	}
-	public static function fatal(){
+	public static function fatal() {
 		return call_user_func_array(array(self::getParent(),'fatal'), func_get_args());
 	}
-	public static function append(){
+	public static function append() {
 		return call_user_func_array(array(self::getParent(),'append'), func_get_args());
 	}
-	public static function reply(){
+	public static function reply() {
 		return call_user_func_array(array(self::getParent(),'reply'), func_get_args());
 	}
-	public static function setIndentation(string $indentation,int $repeat = 1){
+	public static function setIndentation(string $indentation, int $repeat = 1) {
 		self::$indentation = str_repeat($indentation,$repeat);
 	}
-	public static function write($level, $message){
-		$microtime = explode(" ",microtime());
-		$date = date("Y-m-d H:i:s.".substr($microtime[0],2)." P");
-		$levelText = '';
-		switch($level){
-			case(self::debug):$levelText = '[DEBUG]';break;
-			case(self::info):$levelText = '[INFO]';break;
-			case(self::warn):$levelText = '[WARN]';break;
-			case(self::error):$levelText = '[ERROR]';break;
-			case(self::fatal):$levelText = '[FATAL]';break;
+	public static function write($level, $message) {
+		$microtime = explode(" ", microtime());
+		$date = date("Y-m-d H:i:s." . substr($microtime[0], 2) . " P");
+		$coloredMessage = $message;
+		$levelText = "";
+		$coloredLevelText = "";
+		switch($level) {
+			case(self::debug):
+				$levelText = "[DEBUG]";
+				$coloredLevelText = "\033[46m" . $levelText . "\033[0m"; // Background Cyan
+				$coloredMessage = "\033[96m" . $message . "\033[0m"; // Light Cyan
+				break;
+			case(self::info):
+				$levelText = "[INFO]";
+				$coloredLevelText = "\033[42m" . $levelText . "\033[0m"; // Background Green
+				$coloredMessage = "\033[92m" . $message . "\033[0m"; // Light Green
+				break;
+			case(self::warn):
+				$levelText = "[WARN]";
+				$coloredLevelText = "\033[43m" . $levelText . "\033[0m"; // Background Yellow
+				$coloredMessage = "\033[93m" . $message . "\033[0m"; // Light Yellow
+				break;
+			case(self::error):
+				$levelText = "[ERROR]";
+				$coloredLevelText = "\033[45m" . $levelText . "\033[0m"; // Background Magenta
+				$coloredMessage = "\033[95m" . $message . "\033[0m"; // Light Magenta
+				break;
+			case(self::fatal):
+				$levelText = "[FATAL]";
+				$coloredLevelText = "\033[41m" . $levelText . "\033[0m"; // Background Red
+				$coloredMessage = "\033[91m" . $message . "\033[0m"; // Light Red
+				break;
 		}
-		$line = $date." ".$levelText.(self::$generation > 1 ? str_repeat(self::$indentation, self::$generation-1) : ' ').$message."\n";
-		if(options::get('packages.base.logging.quiet', false) == 0){
-			echo $line;
+		$generation = (self::$generation > 1 ? str_repeat(self::$indentation, self::$generation-1) : " ");
+		$coloredLine = $date . " " . $coloredLevelText . $generation . $coloredMessage . "\n";
+		$line = $date . " " . $levelText . $generation . $message . "\n";
+		if (Options::get("packages.base.logging.quiet", false) == 0) {
+			echo $coloredLine;
 		}
 		file_put_contents(self::$file, $line, is_file(self::$file) ? FILE_APPEND : 0);
 	}
