998e4ecb7355b7fa666f84d3b078857e3021a8bf
Amir H. Yeganemehr <ygnmehr@gmail.com>
2024-09-14 19:40:24 +0330

    Handle Some Basic Exception in Controllers

--- src/Routing/ControllerDispatcher.php
+++ src/Routing/ControllerDispatcher.php
@@ -6,6 +6,13 @@ use Illuminate\Routing\ControllerDispatcher as LaravelControllerDispatcher;
 use ReflectionFunctionAbstract;
 use ReflectionNamedType;
 use ReflectionParameter;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Request;
+use packages\base\DB\DuplicateRecord;
+use packages\base\InputValidationException;
+use packages\base\View\Error;
+use packages\base\Views\Form;
+use packages\base\Views\FormError;
 
 class ControllerDispatcher extends LaravelControllerDispatcher
 {
@@ -29,5 +36,37 @@ class ControllerDispatcher extends LaravelControllerDispatcher
 			}
 		}
 		return parent::resolveMethodDependencies($parameters, $reflector);
+	}
+
+	public function dispatch(Route $route, $controller, $method)
+	{
+		try {
+			return parent::dispatch($route, $controller, $method);
+		} catch (InputValidationException | DuplicateRecord | Error $e) {
+			$response = $controller->getResponse();
+			if (!$response) {
+				throw $e;
+			}
+			$response->setStatus(false);
+
+			if (!($e instanceof Error)) {
+				$e = FormError::fromException($e);
+			}
+
+			$view = $response->getView();
+			if ($view) {
+				if ($view instanceof Form) {
+					$view->setFormError($e);
+					$view->setDataForm(Request::instance()->request->all());
+				}
+			} else {
+				$e->setTraceMode(Error::NO_TRACE);
+				$response->setData([
+					'error' => [$e],
+				]);
+			}
+
+			return $response;
+		}
 	}
 }
